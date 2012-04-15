f491e3562e2852b27484bd44bb8c95ae6f2dca47
João Duarte <joao-d-duarte@telecom.pt>
2012-04-16 00:41:43 +0100

    added install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -s ~/{projects/configs/,.}gemrc
+ln -s ~/{projects/configs/,.}gitconfig
+ln -s ~/{projects/configs/,.}irbrc    
+ln -s ~/{projects/configs/,.}vim  
+ln -s ~/{projects/configs/,.}vimrc
+ln -s ~/{projects/configs/,.}zshrc
+ln -s ~/{projects/configs/,.}zshrc.local
